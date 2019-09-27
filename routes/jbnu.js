b99bc302d2f06ceebd96a8f6498e6050fd739027
Deun Lee <54697735+deunlee@users.noreply.github.com>
2019-09-27 21:49:31 +0900

    웹파싱 수정

--- routes/jbnu.js
+++ routes/jbnu.js
@@ -10,58 +10,65 @@ router.get('/', function(req, res, next) {
 });
 
 router.get("/search", function(req, res, next){
+  if (req.query.q === undefined || req.query.length < 3) {
+    res.json([]);
+    return;
+  }
+
   const param = {
-    "q"    : urlencode("화학"),
-    "st"   : "KWRD",
-    "si"   : "TOTAL",
-    "lmtst": "OR",
-    "lmt0" : "TOTAL",
-    "cpp"  : "30",               // 검색 개수
-    "bk_2" : "jttjaa000000jttj", // 중앙도서관
-    "bk_1" : "jttjkorjttj",      // 한국어
-    "bk_0" : "jttjmjttj",        // 단행본
+    q        : urlencode(req.query.q), // 검색어
+    st       : "KWRD",
+    si       : "TOTAL",
+    lmtst    : "OR",
+    lmt0     : "TOTAL",
+    cpp      : "1",               // 검색 개수
+    bk_2     : "jttjaa000000jttj", // 중앙도서관
+    bk_1     : "jttjkorjttj",      // 한국어
+    bk_0     : "jttjmjttj",        // 단행본
+    briefType: "T",                // 테이블 형태
   };
   const url = makeUrl("http://dl.jbnu.ac.kr/search/tot/result", param);
   const headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0"
   };
 
-  request({ url, headers },
-    function(error, response, body) {
+  try {
+    request({ url, headers }, (error, response, body) => {
       const $ = cheerio.load(body);
       const result = [];
-
-      $(".briefData").each(
-        function (index, ele) {
-          if ($(this).attr("id") === "divNoResult") { // 검색 결과가 없는 경우
-            return;
-          }
-
-          let info = $(this).find(".bookline").map(function() {
-            return $(this).text();
-          });
-
-          result[index] = {
-            title     : trim($(this).find(".searchTitle").text()),
-            author    : info[0],
-            publisher : info[1],
-            symbol    : info[2],               // 십진분류 청구기호
-            number    : info[2].split(" ")[0], // 숫자만
-            state     : info[5].includes("중앙도서관 대출가능"),
-          };
-        });
+      //console.log($("#briefTable tbody tr"));
+      $("#briefTable tbody tr").each((index, ele) => {
+        console.log(ele);
+        if ($(this).attr("id") === "divNoResult") { // 검색 결과가 없는 경우
+          return;
+        }
+        console.log($("td:nth-child(3) .searchTitle").text());
+        result[index] = {
+          title     : $("td:nth-child(3) .searchTitle").text(),
+          author    : trimTab($("td:nth-child(4)").text()),
+          publisher : trimTab($("td:nth-child(5)").text()),
+          symbol    : trimTab($("td:nth-child(6)").text()),               // 십진분류 청구기호
+          number    : $("td:nth-child(6)").text().split(" ")[0], // 숫자만
+          state     : $("td:nth-child(3) .briefDeFont").text().includes("중앙도서관 대출가능"), // 대출가능 여부
+        };
+      });
 
       res.json(result); // JSON 출력
     });
+  }
+  catch {
+    res.json(["err"]);
+  }
+
 
 });
 
-let trim = function (str) {
-  return str.replace(/^\s*|\s*$/g, "");
+let trimTab = (str) => {
+  return str.replace(/[\t\n\r]/gm, "").trim(); //str.replace(/^\s*|\s*$/g, "");
 };
 
-let makeUrl = function (url, param) {
-  Object.keys(param).forEach(function(key, index) {
+let makeUrl = (url, param) => {
+  Object.keys(param).forEach((key, index) => {
     url = url + (index === 0 ? "?" : "&") + key + "=" + param[key];
   });
   return url;
