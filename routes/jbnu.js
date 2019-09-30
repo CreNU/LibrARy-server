d382e512ee2db50dc01cc8aa259ab3cb30dc3964
Deun Lee <54697735+deunlee@users.noreply.github.com>
2019-10-01 01:33:15 +0900

    책장 쿼리 추가

--- routes/jbnu.js
+++ routes/jbnu.js
@@ -2,57 +2,59 @@ const express = require('express');
 const router = express.Router();
 
 const request = require('request');
-const cheerio = require('cheerio');
+const cheerio = require('cheerio'); // https://cheerio.js.org/
 const urlencode = require('urlencode');
 
-// cheerio 공식문서 https://cheerio.js.org/
 
 router.get('/', function(req, res, next) {
   res.send('server');
 });
 
-router.get("/search", function(req, res, next){
-  if (req.query.q === undefined || req.query.length < 3) {
+
+router.get('/search', (req, res, next) => {
+  let book_name = req.query.q;
+  if (book_name === undefined || book_name.length < 3) {
     res.json([]);
     return;
   }
   req.connection.setTimeout(1000 * 60); // request가 중복 요청되는 현상 방지
 
+
   const param = {
-    q        : urlencode(req.query.q), // 검색어
-    st       : "KWRD",
-    si       : "TOTAL",
-    lmtst    : "OR",
-    lmt0     : "TOTAL",
-    cpp      : "30",               // 검색 개수
-    bk_2     : "jttjaa000000jttj", // 중앙도서관
-    bk_1     : "jttjkorjttj",      // 한국어
-    bk_0     : "jttjmjttj",        // 단행본
-    briefType: "T",                // 테이블 형태
+    q        : urlencode(book_name), // 검색어
+    st       : 'KWRD',
+    si       : 'TOTAL',
+    lmtst    : 'OR',
+    lmt0     : 'TOTAL',
+    cpp      : '30',               // 검색 개수
+    bk_2     : 'jttjaa000000jttj', // 중앙도서관
+    bk_1     : 'jttjkorjttj',      // 한국어
+    bk_0     : 'jttjmjttj',        // 단행본
+    briefType: 'T',                // 테이블 형태
   };
-  const url = makeUrl("http://dl.jbnu.ac.kr/search/tot/result", param);
+  const url = makeUrl('http://dl.jbnu.ac.kr/search/tot/result', param);
   const headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0"
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0'
   };
 
   request({ url, headers }, (error, response, body) => {
     const $ = cheerio.load(body);
     const result = [];
     
-    $("#briefTable tbody tr").each((i, elem) => {
+    $('#briefTable tbody tr').each((i, elem) => {
       if (i % 2 !== 0) { // 짝수번째 행에는 데이터 없음 (i는 0부터 시작)
         return;
       }
 
       //const info = [];
-      //$(elem).find("td").map((i, elem) => info[i] = $(elem).text()); 
+      //$(elem).find('td').map((i, elem) => info[i] = $(elem).text()); 
 
-      const title     = $(elem).find(".searchTitle").text();;
-      const author    = trimTab($(elem).find("td:nth-child(4)").text());
-      const publisher = trimTab($(elem).find("td:nth-child(5)").text());
-      const symbol    = trimTab($(elem).find("td:nth-child(6)").text()); // 십진분류 청구기호
-      const number    = symbol.split(" ")[0]; // 숫자만
-      const state     = $(elem).find(".briefDeFont").text().includes("중앙도서관 대출가능"); // 대출가능 여부
+      const title     = $(elem).find('.searchTitle').text();;
+      const author    = trimTab($(elem).find('td:nth-child(4)').text());
+      const publisher = trimTab($(elem).find('td:nth-child(5)').text());
+      const symbol    = trimTab($(elem).find('td:nth-child(6)').text()); // 십진분류 청구기호
+      const number    = symbol.split(' ')[0]; // 숫자만
+      const state     = $(elem).find('.briefDeFont').text().includes('중앙도서관 대출가능'); // 대출가능 여부
 
       result.push({
         title,
@@ -71,9 +73,53 @@ router.get("/search", function(req, res, next){
 
 
 
+router.get('/pos', function(req, res, next) {
+  let book_symbol = req.query.q;
+  
+  
+  if (book_symbol === undefined) {
+    res.json([]);
+    return;
+  }
+  book_symbol = book_symbol.replace(/[`~!@#$%^&*()_|+\-=?;:'",<>\{\}\\\/]/gi, '').trim();
+  if (book_symbol.length < 3) {
+    res.json([]);
+    return;
+  }
+
+  const mysql      = require('mysql');
+  const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : '***', // hide for github
+    password : '***',
+    database : 'crenu',
+  });
+  
+  connection.connect();
+  
+  connection.query("SELECT * FROM `shelf_jbnu` WHERE symbol >= '" + book_symbol + "' ORDER BY pos LIMIT 1;", (err, rows, fields) => {
+    if (err) throw err;
+
+    res.json(rows[0]);
+  });
+  
+  connection.end();
+});
+
+
+
+
+
+
+
+
 
 let trimTab = (str) => {
   return str.replace(/[\t\n\r]/gm, "").trim(); //str.replace(/^\s*|\s*$/g, "");
+};
+
+let removeSpecialChars = (str) => {
+  return str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
 };
 
 let makeUrl = (url, param) => {
