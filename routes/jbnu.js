923e3324673754cb6d24971ad562012ca95f9845
Deun Lee <54697735+deunlee@users.noreply.github.com>
2019-10-02 22:57:05 +0900

    쿼리를 하나로 통합

--- routes/jbnu.js
+++ routes/jbnu.js
@@ -11,50 +11,10 @@ const mysql     = require('mysql');
 // 기타
 const common    = require('./common');
 
-
-router.get('/', (req, res, next) => {
-  res.send('server');
-});
-//const info = [];
-//$(elem).find('td').map((i, elem) => info[i] = $(elem).text()); 
-
-const doRequest = (url) => {
-  return new Promise((resolve, reject) => {
-    request(url, (err, res, body) => {
-      if (!err && res.statusCode == 200) {
-        resolve(body);
-      } else {
-        reject(err);
-      }
-    });
-  });
-};
-
-const doQuery = (connection, query) => {
-  return new Promise((resolve, reject) => {
-    console.log(query);
-    connection.query(query, (err, rows, fields) => {
-      if (!err) {
-        resolve(rows[0]);
-      } else {
-        reject(err);
-      }
-    });
-  });
-};
-
-
-
-
-
-
-
-
-
-
-router.get('/search', async (req, res, next) => {
+// 요청 처리
+router.get('/', async (req, res, next) => {
   let book_name = req.query.q;
-  if (book_name === undefined || book_name.length <= 1) { // 쿼리 내용이 없거나 1글자인 경우
+  if (book_name === undefined || book_name.length <= 1) { // 쿼리 내용이 없거나 한 글자인 경우
     res.json([]);
     return;
   }
@@ -88,7 +48,7 @@ router.get('/search', async (req, res, next) => {
       headers,
     });
   } catch (err) {
-    console.log('request failed!');
+    console.log('[FAIL] Failed to request.');
     console.log(err);
     res.json([]);
     return;
@@ -111,63 +71,51 @@ router.get('/search', async (req, res, next) => {
       });
     });
   } catch (err) {
-    console.log('parse failed!');
+    console.log('[FAIL] Cannot parse tags.');
     console.log(err);
     res.json([]);
     return;
   }
 
-  /*
-  const dbConfig = req.app.get('config').database;
-  async function getResult(symbol) {
-    let connection;
-    try {
-      connection = await mysql.createConnection(dbConfig);
-
-      const table  = 'shelf_jbnu';
-      const query  = "SELECT * FROM `" + table + "` WHERE symbol >= '" + symbol + "' ORDER BY pos LIMIT 1;";
-      const result = await connection.query(query);
-
-      console.log(result);
-
-      connection.end();
-
-      result['success'] = true;
-      return result;
-
-    } catch (err) {
-      console.log('db query failed!');
-      if (connection && connection.end) connection.end();
-      return false;
-    }
-  }*/
-
-  const dbConfig = req.app.get('config').database;
-  const dbTable = 'shelf_jbnu';
-  const connection = mysql.createConnection(dbConfig);
-  //const Asymbol = symbol.replace(/[`~!@#$%^&*_|+\-=?;:'",<>\{\}\\\/]/gi, '').trim();
+  // 책 위치 쿼리
+  try {
+    // 데이터베이스 연결
+    const dbConfig = req.app.get('config').database;
+    const dbTable  = 'shelf_jbnu';
+    const dbConn   = mysql.createConnection(dbConfig);
+    dbConn.connect();
   
-  connection.connect();
+    // 책 각각의 위치 조회
+    for (const item of result) {
+      //console.log(item['symbol']);
+      try {
+        const symbol = item['symbol'].replace(/[`~!@#$%^&*_|+\-=?;:'",<>\{\}\\\/]/gi, '').trim();
+        const query  = "SELECT * FROM `" + dbTable + "` WHERE symbol >= '" + symbol + "' ORDER BY pos LIMIT 1;";
+        const pos    = await doQuery(dbConn, query);
 
-  for (const item of result) {
-    console.log(item['symbol']);
-
-    try {
-      const query = "SELECT * FROM `shelf_jbnu` WHERE symbol >= '" + item['symbol'] + "' ORDER BY pos LIMIT 1;";
-
-      const position = await doQuery(connection, query);
-      item['success'] = true;
-      item['pos'] = position;
-    } catch (err) {
-      console.log('db query failed!');
-      console.log(err);
-      item['success'] = false;
+        if (pos.length > 0) { // 위치 정보가 있을 경우
+          item['floor']   = pos[0]['floor'];
+          item['shelf']   = pos[0]['shelf'];
+          item['pos']     = pos[0]['pos'];
+          item['success'] = true;
+        } else {
+          item['success'] = false; // 미등록 책 (위치 모름)
+        }
+      } catch (err) {
+        console.log('db query failed!');
+        console.log(err);
+        item['success'] = false; // 디비 조회 실패
+      }
     }
-    
-  }
-  
-  if (connection && connection.end) {
-    connection.end();
+    // 닫기
+    if (dbConn && dbConn.end) {
+      dbConn.end();
+    }
+  } catch (err) {
+    console.log('[FAIL] Cannot retrieve book\'s position.');
+    console.log(err);
+    res.json([]);
+    return;
   }
 
 
@@ -178,66 +126,28 @@ router.get('/search', async (req, res, next) => {
 
 
 
-
-
-
-
-
-
-router.get('/pos', function(req, res, next) {
-  const null_info = {
-    floor: -1,
-    shelf: -1,
-    dir: -1,
-    pos: -1,
-  }
-
-  let book_symbol = req.query.q;
-  if (book_symbol === undefined) {
-    res.json(null_info);
-    return;
-  }
-  // prevent sql injection
-  book_symbol = book_symbol.replace(/[`~!@#$%^&*_|+\-=?;:'",<>\{\}\\\/]/gi, '').trim();
-  if (book_symbol.length < 3) {
-    res.json(null_info);
-    return;
-  }
-
-  const mysql      = require('mysql');
-  const login_info = req.app.get('config').database;
-  const connection = mysql.createConnection(login_info);
-  
-  const query = "SELECT * FROM `shelf_jbnu` WHERE symbol >= '" + book_symbol + "' ORDER BY pos LIMIT 1;";
-
-  connection.connect();
-  connection.query(query, (err, rows, fields) => {
-    if (err) throw err;
-
-    if (rows.length === 0) {
-      res.json(null_info);
-    } else {
-      res.json(rows[0]);
-    }
+const doRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (!err && res.statusCode == 200) {
+        resolve(body);
+      } else {
+        reject(err);
+      }
+    });
   });
-  
-  connection.end();
-});
-
-
-
-
-const getConnection = async (pool) => {
-  try {
-    const connection = await pool.getConnection(async conn => conn);
-    return connection;
-  } catch(err) {
-    console.log('DB Error!');
-    return false;
-  }
 };
 
-
-
+const doQuery = (connection, query) => {
+  return new Promise((resolve, reject) => {
+    connection.query(query, (err, rows, fields) => {
+      if (!err) {
+        resolve(rows);
+      } else {
+        reject(err);
+      }
+    });
+  });
+};
 
 module.exports = router;
